9aedb86825f8515e42653966b134eb0463a2a94b
MC Kwit <mckwit@gmail.com>
2015-05-08 14:38:07 -0400

    Update rPlots.r

--- rPlots.r
+++ rPlots.r
@@ -59,3 +59,90 @@ pictogram <- function(data,size.gap = 3, dtree, ctree = NULL,aspect = .7,Main.Ti
   
 }
 
+###
+##
+###
+
+squarePie <- function(data,size.gap = 10,Main.Title = 'Main Title',Sub.Title=NA,Unit.Name = 'Units',nh = 7,nw = 34){ #plot a pictogram
+  
+  #data: vector of counts with names
+  #size.gap: fraction of column height is gap between squares, fraction = icon.height/size.gap
+  #nh: number of squares high
+  #nw: number of squares wide
+
+  data = rev(sort(data))
+  ndata = length(data)
+  
+  colours = c("#00A0B0","#6A4A3C","#CC333F","#EDC951","#37C040")
+  colours = colorRange(percent = seq(0,1,length=ndata+1),colours = colours,trans = 1)
+  width = .7
+  height = width/4
+  n = nh*nw
+  unit = round(sum(data,na.rm=T) / n)
+  allNum = round(data/unit)
+  cumNum = cumsum(allNum)
+  
+  iSide=min(c(height/nh,width/nw))
+  gap = iSide / size.gap
+  
+  iSide = iSide - gap
+  shift = iSide + gap  
+  
+  xs = c(shift,shift+iSide)
+  ys = c(.45,.45+iSide)
+  dotted = c(1,ceiling(cumNum/nh)+1)
+
+  par(mar=c(0, 0, 0, 0), oma=c(0,0,0,0), bg="white", xpd=FALSE, xaxs="r", yaxs="i", mgp=c(2.1,.3,0), las=1, col.axis="#434343", col.main="#343434", tck=0, lend=1)
+  plot(0,0,xlim=c(0,1),ylim=c(0,1),type='n',xaxt='n',yaxt='n',bty='n',las=1,xlab="", ylab="", family="Helvetica", cex.main=1.5, cex.axis=0.8, cex.lab=0.8)
+  rect(xleft = 0, xright = shift*(nw + .4*nw) + xs[1], 
+       ybottom = ys[1] - shift*3.5 - 2*strheight(names(data)[1],cex=.75), ytop =  shift*(nh) + ys[2] + strheight(Main.Title) + shift,
+       col = "#E8F4F8" ,border="#000026")
+  k = 0;l=1
+  for(j in 1:(nw+3)){
+    if(j==dotted[l]){
+      if(l %% 3 == 0){mul = 2.6}else{if(l %% 3 == 2){mul = 1.3}else{mul = 0}}
+      lines(x = rep(xs[1]+shift*(j-1+.5),2),y = c(ys[1]-shift*.1,ys[1] - shift*0.8 - mul*strheight(names(data)[1],cex=.75)),lty=3,lwd=1,col="#000026")
+      text(xs[1]+shift*(j-1),ys[1] - shift - mul*strheight(names(data)[1],cex=.75),names(data)[l],cex=.75,adj=c(0,.9),col="#000026")
+      l=l+1
+    }
+    for(i in 1:nh){
+      k = k + 1
+      colNum = rep(1:ndata,allNum)
+      rect(xleft=xs[1]+shift*(j-1),xright=xs[2]+shift*(j-1),ybottom=ys[1]+shift*(i-1),ytop=ys[2]+shift*(i-1),
+           col = colours[colNum[k]] ,border="#E8F4F8") # single pack 
+      if(k == sum(allNum))break
+    }
+    if(k == sum(allNum))break  
+  }
+  text(shift, 
+       shift*(nh) + ys[2] ,
+       labels = Main.Title,
+       pos=4,offset=0,col="#000026")
+  
+  sub.width = (shift*(nw + .4*nw) + xs[1]) - (xs[2]+shift*(j-1)) - shift
+  if(is.na(Sub.Title)){
+    Sub.Title = str_wrap(paste(prettyNum(sum(data,na.rm=T),big.mark=",",scientific=F),'total', Unit.Name), width = floor(2*sub.width/strwidth('me', cex = 1)), indent = 0, exdent = 0)
+  }else{
+    Sub.Title = str_wrap(Sub.Title, width = floor(2*sub.width/strwidth('me', cex = 1)), indent = 0, exdent = 0)
+  }
+  text(shift*(nw + .4*nw) + xs[1] - shift, 
+       shift*(nh) + ys[2] + strheight(Main.Title),
+       labels = Sub.Title,
+       adj=c(1,1),col="#000026")
+  
+  rect(xleft =  (shift*(nw + .4*nw) + xs[1]) - (2*shift + strwidth(paste(unit,Unit.Name))/2) ,
+       xright = (shift*(nw + .4*nw) + xs[1]) - (shift + strwidth(paste(unit,Unit.Name))/2),
+       ybottom=ys[1],ytop=ys[2],
+       col = colours[ndata+1] ,border="#E8F4F8") 
+  lines(x = rep((shift*(nw + .4*nw) + xs[1]) - (1.5*shift + strwidth(paste(unit,Unit.Name))/2),2),
+        y = c(ys[1]-shift*.1,ys[1] - shift*.8),lty=3,lwd=1,col="#000026")
+  text(x = (shift*(nw + .4*nw) + xs[1]) - (1.5*shift + strwidth(paste(unit,Unit.Name))/2),
+       y = ys[1] - shift,
+       paste(prettyNum(unit,big.mark=",",scientific=F),Unit.Name),cex=.75,col="#000026")
+  
+  
+}
+
+
+
+
